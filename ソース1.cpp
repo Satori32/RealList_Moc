3ded2cdc8bf7e945ef36f7366587728862e62ad7
Satori32 <Huga8989+githubf@gmail.com>
2024-03-16 10:08:23 +0900

    add comment

--- ソース1.cpp
+++ ソース1.cpp
@@ -39,6 +39,72 @@ protected:
 	double X = 0;
 };
 
+//god or faity is not use è¨êî on culc time. by histry.
+//but fairy is use it. but it time is facebreakable time.so he hate it
+
+template<class T>
+class List2 {
+public:
+	typedef std::map <double, T > List_T;//world spec thing holder
+
+	bool forWard() {
+		X += M;
+		return true;
+	}
+	bool BackWard() {
+		X -= M;
+		return true;
+	}
+	T& Take() {
+		return L[X / M];
+	}
+
+	bool SetM(double V) {
+		M = V;
+		return true;
+	}
+
+	T& operator [](double In) {
+		X += In;//why this need?
+		return L[(X / M)];
+	}
+
+protected:
+	List_T L;
+	std::uintmax_t M = 10;
+	std::uintmax_t X = 0;
+};
+template<class T>
+class ListYaki {
+public:
+	//typedef std::map <double, T > List_T;//world spec thing holder
+	typedef std::map <std::intmax_t, T > List_T;//refine and detune
+	bool forWard() {
+		X += M
+		return true;
+	}
+	bool BackWard() {
+		X -= M;
+		return true;
+	}
+	T& Take() {
+		return L[X / M];
+	}
+
+	bool SetM(double V) {
+		M = V;
+		return true;
+	}
+
+	T& operator [](double In) {
+		return L[(X / M)];
+	}
+
+protected:
+	List_T L;
+	std::uintmax_t M = 10;
+	std::uintmax_t X = 0;
+};
 int main() {
 	List<int> L;
 
